b67b64168c6d303777fcd88e3d6eb66197ff232d
mingo <mingo233@qq.com>
2022-06-27 21:13:50 +0800

    init

---
+++ vite/vite-project/src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2 :class="style.border">{{ msg }}</h2>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+import style from "./test.module.less";
+
+const msg = ref("");
+const handle = () => {
+  console.log("handle");
+};
+console.log(msg);
+
+msg.value = "nihao";
+</script>
+
+<style lang="less" scoped>
+// @themeColor: #8de;
+h2 {
+  //   color: rebeccapurple;
+  color: @themeColor;
+}
+</style>
